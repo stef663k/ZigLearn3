efba5337889bea0efc2826f6d181d53e53a1f9ad
stef663k <stef663k@elev.techcollege.dk>
2023-03-02 11:18:20 +0100

    Opgave 19

--- src/main.zig
+++ src/main.zig
@@ -242,20 +242,66 @@
 // ??? deepThought() ??? {
 //     return 42; // Number courtesy Douglas Adams
 // }
+// const std = @import("std");
+
+// pub fn main() void {
+//     // The new function deepThought() should return the number 42. See below.
+//     const answer: u8 = deepThought();
+
+//     std.debug.print("Answer to the Ultimate Question: {}\n", .{answer});
+// }
+
+// // Please define the deepThought() function below.
+// //
+// // We're just missing a couple things. One thing we're NOT missing is the
+// // keyword "pub", which is not needed here. Can you guess why?
+// //
+// fn deepThought() u8 {
+//     return 42; // Number courtesy Douglas Adams
+// }
+
+///Opgave 19
+//
+// Now let's create a function that takes a parameter. Here's an
+// example that takes two parameters. As you can see, parameters
+// are declared just like any other types ("name": "type"):
+//
+//     fn myFunction(number: u8, is_lucky: bool) {
+//         ...
+//     }
+//
+// const std = @import("std");
+
+// pub fn main() void {
+//     std.debug.print("Powers of two: {} {} {} {}\n", .{
+//         twoToThe(1),
+//         twoToThe(2),
+//         twoToThe(3),
+//         twoToThe(4),
+//     });
+// }
+
+// // Please give this function the correct input parameter(s).
+// // You'll need to figure out the parameter name and type that we're
+// // expecting. The output type has already been specified for you.
+// //
+// fn twoToThe(???) u32 {
+//     return std.math.pow(u32, 2, my_number);
+//     // std.math.pow(type, a, b) takes a numeric type and two
+//     // numbers of that type (or that can coerce to that type) and
+//     // returns "a to the power of b" as that same numeric type.
+// }
 const std = @import("std");
 
 pub fn main() void {
-    // The new function deepThought() should return the number 42. See below.
-    const answer: u8 = deepThought();
-
-    std.debug.print("Answer to the Ultimate Question: {}\n", .{answer});
+    std.debug.print("Powers of two: {} {} {} {}\n", .{
+        twoToThe(1),
+        twoToThe(2),
+        twoToThe(3),
+        twoToThe(4),
+    });
 }
 
-// Please define the deepThought() function below.
-//
-// We're just missing a couple things. One thing we're NOT missing is the
-// keyword "pub", which is not needed here. Can you guess why?
-//
-fn deepThought() u8 {
-    return 42; // Number courtesy Douglas Adams
+fn twoToThe(my_number: u32) u32 {
+    return std.math.pow(u32, 2, my_number);
 }
