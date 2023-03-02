7c0724d8fa77b89ad9e09566704f425a583401c4
stef663k <stef663k@elev.techcollege.dk>
2023-03-02 07:42:36 +0100

    Opgave 15

--- src/main.zig
+++ src/main.zig
@@ -44,17 +44,61 @@
 //     // Result: we want n=4
 //     std.debug.print("n={}\n", .{n});
 // }
+// const std = @import("std");
+
+// pub fn main() void {
+//     var n: u32 = 1;
+
+//     // Oh dear! This while loop will go forever?!
+//     // Please fix this so the print statement below gives the desired output.
+//     while (true) : (n += 1) {
+//         if (n == 4) break;
+//     }
+
+//     // Result: we want n=4
+//     std.debug.print("n={}\n", .{n});
+// }
+
+///Opgave 15
+//
+// Behold the 'for' loop! For loops let you execute code for each
+// element of an array:
+//
+//     for (items) |item| {
+//
+//         // Do something with item
+//
+//     }
+//
+// const std = @import("std");
+
+// pub fn main() void {
+//     const story = [_]u8{ 'h', 'h', 's', 'n', 'h' };
+
+//     std.debug.print("A Dramatic Story: ", .{});
+
+//     for (???) |???| {
+//         if (scene == 'h') std.debug.print(":-)  ", .{});
+//         if (scene == 's') std.debug.print(":-(  ", .{});
+//         if (scene == 'n') std.debug.print(":-|  ", .{});
+//     }
+
+//     std.debug.print("The End.\n", .{});
+// }
+// Note that "for" loops also work on things called "slices"
+// which we'll see later.
 const std = @import("std");
 
 pub fn main() void {
-    var n: u32 = 1;
+    const story = [_]u8{ 'h', 'h', 's', 'n', 'h' };
 
-    // Oh dear! This while loop will go forever?!
-    // Please fix this so the print statement below gives the desired output.
-    while (true) : (n += 1) {
-        if (n == 4) break;
+    std.debug.print("A Dramatic Story: ", .{});
+
+    for (story) |scene| {
+        if (scene == 'h') std.debug.print(":-)  ", .{});
+        if (scene == 's') std.debug.print(":-(  ", .{});
+        if (scene == 'n') std.debug.print(":-|  ", .{});
     }
 
-    // Result: we want n=4
-    std.debug.print("n={}\n", .{n});
+    std.debug.print("The End.\n", .{});
 }
