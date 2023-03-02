72f6c268989421a78637b3f2d54ff0d1106ea3ba
stef663k <stef663k@elev.techcollege.dk>
2023-03-02 07:51:53 +0100

    Opgave 16

--- src/main.zig
+++ src/main.zig
@@ -87,18 +87,79 @@
 // }
 // Note that "for" loops also work on things called "slices"
 // which we'll see later.
+// const std = @import("std");
+
+// pub fn main() void {
+//     const story = [_]u8{ 'h', 'h', 's', 'n', 'h' };
+
+//     std.debug.print("A Dramatic Story: ", .{});
+
+//     for (story) |scene| {
+//         if (scene == 'h') std.debug.print(":-)  ", .{});
+//         if (scene == 's') std.debug.print(":-(  ", .{});
+//         if (scene == 'n') std.debug.print(":-|  ", .{});
+//     }
+
+//     std.debug.print("The End.\n", .{});
+// }
+
+///Opgave 16
+//
+// For loops also let you use the "index" of the iteration, a number
+// that counts up with each iteration. To access the index of iteration,
+// specify a second condition as well as a second capture value.
+//
+//     for (items, 0..) |item, index| {
+//
+//         // Do something with item and index
+//
+//     }
+//
+// You can name "item" and "index" anything you want. "i" is a popular
+// shortening of "index". The item name is often the singular form of
+// the items you're looping through.
+//
+// const std = @import("std");
+
+// pub fn main() void {
+//     // Let's store the bits of binary number 1101 in
+//     // 'little-endian' order (least significant byte first):
+//     const bits = [_]u8{ 1, 0, 1, 1 };
+//     var value: u32 = 0;
+
+//     // Now we'll convert the binary bits to a number value by adding
+//     // the value of the place as a power of two for each bit.
+//     //
+//     // See if you can figure out the missing pieces:
+//     for (bits, ???) |bit, ???| {
+//         // Note that we convert the usize i to a u32 with
+//         // @intCast(), a builtin function just like @import().
+//         // We'll learn about these properly in a later exercise.
+//         var place_value = std.math.pow(u32, 2, @intCast(u32, i));
+//         value += place_value * bit;
+//     }
+
+//     std.debug.print("The value of bits '1101': {}.\n", .{value});
+// }
 const std = @import("std");
 
 pub fn main() void {
-    const story = [_]u8{ 'h', 'h', 's', 'n', 'h' };
+    // Let's store the bits of binary number 1101 in
+    // 'little-endian' order (least significant byte first):
+    const bits = [_]u8{ 1, 0, 1, 1 };
+    var value: u32 = 0;
 
-    std.debug.print("A Dramatic Story: ", .{});
-
-    for (story) |scene| {
-        if (scene == 'h') std.debug.print(":-)  ", .{});
-        if (scene == 's') std.debug.print(":-(  ", .{});
-        if (scene == 'n') std.debug.print(":-|  ", .{});
+    // Now we'll convert the binary bits to a number value by adding
+    // the value of the place as a power of two for each bit.
+    //
+    // See if you can figure out the missing pieces:
+    for (bits, 0..) |bit, i| {
+        // Note that we convert the usize i to a u32 with
+        // @intCast(), a builtin function just like @import().
+        // We'll learn about these properly in a later exercise.
+        var place_value = std.math.pow(u32, 2, @intCast(u32, i));
+        value += place_value * bit;
     }
 
-    std.debug.print("The End.\n", .{});
+    std.debug.print("The value of bits '1101': {}.\n", .{value});
 }
