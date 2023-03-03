1500c7a05bc259be76c66cee316599d53e15863e
stef663k <stef663k@elev.techcollege.dk>
2023-03-03 07:41:39 +0100

    Opgave 20

--- src/main.zig
+++ src/main.zig
@@ -291,17 +291,88 @@
 //     // numbers of that type (or that can coerce to that type) and
 //     // returns "a to the power of b" as that same numeric type.
 // }
+// const std = @import("std");
+
+// pub fn main() void {
+//     std.debug.print("Powers of two: {} {} {} {}\n", .{
+//         twoToThe(1),
+//         twoToThe(2),
+//         twoToThe(3),
+//         twoToThe(4),
+//     });
+// }
+
+// fn twoToThe(my_number: u32) u32 {
+//     return std.math.pow(u32, 2, my_number);
+// }
+
+///Opgave 20
+//
+// Let's see if we can make use of some of the things we've learned so far.
+// We'll create two functions: one that contains a "for" loop and one
+// that contains a "while" loop.
+//
+// Both of these are simply labeled "loop" below.
+//
+// const std = @import("std");
+
+// pub fn main() void {
+//     const my_numbers = [4]u16{ 5, 6, 7, 8 };
+
+//     printPowersOfTwo(my_numbers);
+//     std.debug.print("\n", .{});
+// }
+
+// // You won't see this every day: a function that takes an array with
+// // exactly four u16 numbers. This is not how you would normally pass
+// // an array to a function. We'll learn about slices and pointers in
+// // a little while. For now, we're using what we know.
+// //
+// // This function prints, but does not return anything.
+// //
+// fn printPowersOfTwo(numbers: [4]u16) ??? {
+//     loop (numbers) |n| {
+//         std.debug.print("{} ", .{twoToThe(n)});
+//     }
+// }
+
+// // This function bears a striking resemblance to twoToThe() in the last
+// // exercise. But don't be fooled! This one does the math without the aid
+// // of the standard library!
+// //
+// fn twoToThe(number: u16) ??? {
+//     var n: u16 = 0;
+//     var total: u16 = 1;
+
+//     loop (n < number) : (n += 1) {
+//         total *= 2;
+//     }
+
+//     return ???;
+// }
+
 const std = @import("std");
 
 pub fn main() void {
-    std.debug.print("Powers of two: {} {} {} {}\n", .{
-        twoToThe(1),
-        twoToThe(2),
-        twoToThe(3),
-        twoToThe(4),
-    });
+    const my_numbers = [4]u16{ 5, 6, 7, 8 };
+
+    printPowersOfTwo(my_numbers);
+    std.debug.print("\n", .{});
 }
 
-fn twoToThe(my_number: u32) u32 {
-    return std.math.pow(u32, 2, my_number);
+fn printPowersOfTwo(numbers: [4]u16) void {
+    for (numbers) |n| {
+        std.debug.print("{} ", .{twoToThe(n)});
+    }
+}
+
+fn twoToThe(number: u16) u16 {
+    var n: u16 = 0;
+    var total: u16 = 1;
+
+    while (n < number) : (n += 1) {
+        total *= 2;
+    }
+
+    return total;
 }
