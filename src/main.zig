42527690fd38a6ecbce8d9a58254058d333d4d69
stef663k <stef663k@elev.techcollege.dk>
2023-03-03 12:00:29 +0100

    Opgave 21

--- src/main.zig
+++ src/main.zig
@@ -351,28 +351,113 @@
 //     return ???;
 // }
 
+// const std = @import("std");
+
+// pub fn main() void {
+//     const my_numbers = [4]u16{ 5, 6, 7, 8 };
+
+//     printPowersOfTwo(my_numbers);
+//     std.debug.print("\n", .{});
+// }
+
+// fn printPowersOfTwo(numbers: [4]u16) void {
+//     for (numbers) |n| {
+//         std.debug.print("{} ", .{twoToThe(n)});
+//     }
+// }
+
+// fn twoToThe(number: u16) u16 {
+//     var n: u16 = 0;
+//     var total: u16 = 1;
+
+//     while (n < number) : (n += 1) {
+//         total *= 2;
+//     }
+
+//     return total;
+// }
+
+///Opgave 21
+//
+// Believe it or not, sometimes things go wrong in programs.
+//
+// In Zig, an error is a value. Errors are named so we can identify
+// things that can go wrong. Errors are created in "error sets", which
+// are just a collection of named errors.
+//
+// We have the start of an error set, but we're missing the condition
+// "TooSmall". Please add it where needed!
+// const MyNumberError = error{
+//     TooBig,
+//     ???,
+//     TooFour,
+// };
+
+// const std = @import("std");
+
+// pub fn main() void {
+//     const nums = [_]u8{ 2, 3, 4, 5, 6 };
+
+//     for (nums) |n| {
+//         std.debug.print("{}", .{n});
+
+//         const number_error = numberFail(n);
+
+//         if (number_error == MyNumberError.TooBig) {
+//             std.debug.print(">4. ", .{});
+//         }
+//         if (???) {
+//             std.debug.print("<4. ", .{});
+//         }
+//         if (number_error == MyNumberError.TooFour) {
+//             std.debug.print("=4. ", .{});
+//         }
+//     }
+
+//     std.debug.print("\n", .{});
+// }
+
+// // Notice how this function can return any member of the MyNumberError
+// // error set.
+// fn numberFail(n: u8) MyNumberError {
+//     if (n > 4) return MyNumberError.TooBig;
+//     if (n < 4) return MyNumberError.TooSmall; // <---- this one is free!
+//     return MyNumberError.TooFour;
+// }
+const MyNumberError = error{
+    TooBig,
+    TooSmall,
+    TooFour,
+};
+
 const std = @import("std");
 
 pub fn main() void {
-    const my_numbers = [4]u16{ 5, 6, 7, 8 };
+    const nums = [_]u8{ 2, 3, 4, 5, 6 };
 
-    printPowersOfTwo(my_numbers);
+    for (nums) |n| {
+        std.debug.print("{}", .{n});
+
+        const number_error = numberFail(n);
+
+        if (number_error == MyNumberError.TooBig) {
+            std.debug.print(">4. ", .{});
+        }
+        if (number_error == MyNumberError.TooSmall) {
+            std.debug.print("<4. ", .{});
+        }
+        if (number_error == MyNumberError.TooFour) {
+            std.debug.print("=4. ", .{});
+        }
+    }
+
     std.debug.print("\n", .{});
 }
 
-fn printPowersOfTwo(numbers: [4]u16) void {
-    for (numbers) |n| {
-        std.debug.print("{} ", .{twoToThe(n)});
-    }
-}
-
-fn twoToThe(number: u16) u16 {
-    var n: u16 = 0;
-    var total: u16 = 1;
-
-    while (n < number) : (n += 1) {
-        total *= 2;
-    }
-
-    return total;
+// Notice how this function can return any member of the MyNumberError
+// error set.
+fn numberFail(n: u8) MyNumberError {
+    if (n > 4) return MyNumberError.TooBig;
+    if (n < 4) return MyNumberError.TooSmall; // <---- this one is free!
+    return MyNumberError.TooFour;
 }
