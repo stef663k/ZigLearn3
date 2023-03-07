5078dbfe2a9e2e016e26fc57a92618cabe02ebef
stef663k <stef663k@elev.techcollege.dk>
2023-03-07 08:04:28 +0100

    Opgave 26

--- src/main.zig
+++ src/main.zig
@@ -709,32 +709,66 @@
 //     if (n > 20) return MyNumberError.TooBig;
 //     return n;
 // }
+// const std = @import("std");
+
+// const MyNumberError = error{
+//     TooSmall,
+//     TooBig,
+// };
+
+// pub fn main() void {
+//     var a: u32 = addFive(44) catch 0;
+//     var b: u32 = addFive(14) catch 0;
+//     var c: u32 = addFive(4) catch 0;
+
+//     std.debug.print("a={}, b={}, c={}\n", .{ a, b, c });
+// }
+
+// fn addFive(n: u32) MyNumberError!u32 {
+//     // This function needs to return any error which might come back from detect().
+//     // Please use a "try" statement rather than a "catch".
+//     //
+//     var x = try detect(n);
+
+//     return x + 5;
+// }
+
+// fn detect(n: u32) MyNumberError!u32 {
+//     if (n < 10) return MyNumberError.TooSmall;
+//     if (n > 20) return MyNumberError.TooBig;
+//     return n;
+// }
+
+//Opgave 26
+//
+// Great news! Now we know enough to understand a "real" Hello World
+// program in Zig - one that uses the system Standard Out resource...which
+// can fail!
+//
 const std = @import("std");
 
-const MyNumberError = error{
-    TooSmall,
-    TooBig,
-};
+// Take note that this main() definition now returns "!void" rather
+// than just "void". Since there's no specific error type, this means
+// that Zig will infer the error type. This is appropriate in the case
+// of main(), but can make a function harder (function pointers) or
+// even impossible to work with (recursion) in some situations.
+//
+// You can find more information at:
+// https://ziglang.org/documentation/master/#Inferred-Error-Sets
+//
+// pub fn main() !void {
+//     // We get a Writer for Standard Out so we can print() to it.
+//     const stdout = std.io.getStdOut().writer();
 
-pub fn main() void {
-    var a: u32 = addFive(44) catch 0;
-    var b: u32 = addFive(14) catch 0;
-    var c: u32 = addFive(4) catch 0;
+//     // Unlike std.debug.print(), the Standard Out writer can fail
+//     // with an error. We don't care _what_ the error is, we want
+//     // to be able to pass it up as a return value of main().
+//     //
+//     // We just learned of a single statement which can accomplish this.
+//     stdout.print("Hello world!\n", .{});
+// }
+pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
 
-    std.debug.print("a={}, b={}, c={}\n", .{ a, b, c });
-}
-
-fn addFive(n: u32) MyNumberError!u32 {
-    // This function needs to return any error which might come back from detect().
-    // Please use a "try" statement rather than a "catch".
-    //
-    var x = try detect(n);
-
-    return x + 5;
-}
-
-fn detect(n: u32) MyNumberError!u32 {
-    if (n < 10) return MyNumberError.TooSmall;
-    if (n > 20) return MyNumberError.TooBig;
-    return n;
+    try stdout.print("Hello world!\n", .{});
 }
