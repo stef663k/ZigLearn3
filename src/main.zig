f47c8ee9f35f94d444e0cdd1bfd8c0bcfca6f070
stef663k <stef663k@elev.techcollege.dk>
2023-03-07 08:49:01 +0100

    Opgave 28

--- src/main.zig
+++ src/main.zig
@@ -799,9 +799,78 @@
 //     std.debug.print("Two\n", .{});
 //     std.debug.print("One ", .{});
 // }
+// const std = @import("std");
+
+// pub fn main() void {
+//     defer std.debug.print("Two\n", .{});
+//     std.debug.print("One ", .{});
+// }
+
+//Opgave 28
+//
+// Now that you know how "defer" works, let's do something more
+// interesting with it.
+//
+// const std = @import("std");
+
+// pub fn main() void {
+//     const animals = [_]u8{ 'g', 'c', 'd', 'd', 'g', 'z' };
+
+//     for (animals) |a| printAnimal(a);
+
+//     std.debug.print("done.\n", .{});
+// }
+
+// // This function is _supposed_ to print an animal name in parentheses
+// // like "(Goat) ", but we somehow need to print the end parenthesis
+// // even though this function can return in four different places!
+// fn printAnimal(animal: u8) void {
+//     std.debug.print("(", .{});
+
+//     std.debug.print(") ", .{}); // <---- how?!
+
+//     if (animal == 'g') {
+//         std.debug.print("Goat", .{});
+//         return;
+//     }
+//     if (animal == 'c') {
+//         std.debug.print("Cat", .{});
+//         return;
+//     }
+//     if (animal == 'd') {
+//         std.debug.print("Dog", .{});
+//         return;
+//     }
+
+//     std.debug.print("Unknown", .{});
+// }
 const std = @import("std");
 
 pub fn main() void {
-    defer std.debug.print("Two\n", .{});
-    std.debug.print("One ", .{});
+    const animals = [_]u8{ 'g', 'c', 'd', 'd', 'g', 'z' };
+
+    for (animals) |a| printAnimal(a);
+
+    std.debug.print("done.\n", .{});
+}
+
+fn printAnimal(animal: u8) void {
+    std.debug.print("(", .{});
+
+    defer std.debug.print(") ", .{});
+
+    if (animal == 'g') {
+        std.debug.print("Goat", .{});
+        return;
+    }
+    if (animal == 'c') {
+        std.debug.print("Cat", .{});
+        return;
+    }
+    if (animal == 'd') {
+        std.debug.print("Dog", .{});
+        return;
+    }
+
+    std.debug.print("Unknown", .{});
 }
