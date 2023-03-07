ad2c284274036544bac3b6802d903eeb113459b7
stef663k <stef663k@elev.techcollege.dk>
2023-03-07 09:05:07 +0100

    Opgave 29

--- src/main.zig
+++ src/main.zig
@@ -844,33 +844,132 @@
 
 //     std.debug.print("Unknown", .{});
 // }
+// const std = @import("std");
+
+// pub fn main() void {
+//     const animals = [_]u8{ 'g', 'c', 'd', 'd', 'g', 'z' };
+
+//     for (animals) |a| printAnimal(a);
+
+//     std.debug.print("done.\n", .{});
+// }
+
+// fn printAnimal(animal: u8) void {
+//     std.debug.print("(", .{});
+
+//     defer std.debug.print(") ", .{});
+
+//     if (animal == 'g') {
+//         std.debug.print("Goat", .{});
+//         return;
+//     }
+//     if (animal == 'c') {
+//         std.debug.print("Cat", .{});
+//         return;
+//     }
+//     if (animal == 'd') {
+//         std.debug.print("Dog", .{});
+//         return;
+//     }
+
+//     std.debug.print("Unknown", .{});
+// }
+
+//Opgave 29
+//
+// Another common problem is a block of code that could exit in multiple
+// places due to an error - but that needs to do something before it
+// exits (typically to clean up after itself).
+//
+// An "errdefer" is a defer that only runs if the block exits with an error:
+//
+//     {
+//         errdefer cleanup();
+//         try canFail();
+//     }
+//
+// The cleanup() function is called ONLY if the "try" statement returns an
+// error produced by canFail().
+//
+// const std = @import("std");
+
+// var counter: u32 = 0;
+
+// const MyErr = error{ GetFail, IncFail };
+
+// pub fn main() void {
+//     // We simply quit the entire program if we fail to get a number:
+//     var a: u32 = makeNumber() catch return;
+//     var b: u32 = makeNumber() catch return;
+
+//     std.debug.print("Numbers: {}, {}\n", .{ a, b });
+// }
+
+// fn makeNumber() MyErr!u32 {
+//     std.debug.print("Getting number...", .{});
+
+//     // Please make the "failed" message print ONLY if the makeNumber()
+//     // function exits with an error:
+//     std.debug.print("failed!\n", .{});
+
+//     var num = try getNumber(); // <-- This could fail!
+
+//     num = try increaseNumber(num); // <-- This could ALSO fail!
+
+//     std.debug.print("got {}. ", .{num});
+
+//     return num;
+// }
+
+// fn getNumber() MyErr!u32 {
+//     // I _could_ fail...but I don't!
+//     return 4;
+// }
+
+// fn increaseNumber(n: u32) MyErr!u32 {
+//     // I fail after the first time you run me!
+//     if (counter > 0) return MyErr.IncFail;
+
+//     // Sneaky, weird global stuff.
+//     counter += 1;
+
+//     return n + 1;
+// }
 const std = @import("std");
 
+var counter: u32 = 0;
+
+const MyErr = error{ GetFail, IncFail };
+
 pub fn main() void {
-    const animals = [_]u8{ 'g', 'c', 'd', 'd', 'g', 'z' };
+    var a: u32 = makeNumber() catch return;
+    var b: u32 = makeNumber() catch return;
 
-    for (animals) |a| printAnimal(a);
-
-    std.debug.print("done.\n", .{});
+    std.debug.print("Numbers: {}, {}\n", .{ a, b });
 }
 
-fn printAnimal(animal: u8) void {
-    std.debug.print("(", .{});
+fn makeNumber() MyErr!u32 {
+    std.debug.print("Getting number...", .{});
 
-    defer std.debug.print(") ", .{});
+    errdefer std.debug.print("failed!\n", .{});
 
-    if (animal == 'g') {
-        std.debug.print("Goat", .{});
-        return;
-    }
-    if (animal == 'c') {
-        std.debug.print("Cat", .{});
-        return;
-    }
-    if (animal == 'd') {
-        std.debug.print("Dog", .{});
-        return;
-    }
+    var num = try getNumber();
 
-    std.debug.print("Unknown", .{});
+    num = try increaseNumber(num);
+
+    std.debug.print("got {}. ", .{num});
+
+    return num;
+}
+
+fn getNumber() MyErr!u32 {
+    return 4;
+}
+
+fn increaseNumber(n: u32) MyErr!u32 {
+    if (counter > 0) return MyErr.IncFail;
+
+    counter += 1;
+
+    return n + 1;
 }
