f344c226119deb9d80e2eb5b9b2a88e48ce7e628
stef663k <stef663k@elev.techcollege.dk>
2023-03-07 07:47:59 +0100

    Opgave 23

--- src/main.zig
+++ src/main.zig
@@ -492,16 +492,64 @@
 
 //     std.debug.print("I compiled!\n", .{});
 // }
+// const std = @import("std");
+
+// const MyNumberError = error{TooSmall};
+
+// pub fn main() void {
+//     var my_number: MyNumberError!u8 = 5;
+
+//     // Looks like my_number will need to either store a number OR
+//     // an error. Can you set the type correctly above?
+//     my_number = MyNumberError.TooSmall;
+
+//     std.debug.print("I compiled!\n", .{});
+// }
+
+//Opgave 23
+//
+// One way to deal with error unions is to "catch" any error and
+// replace it with a default value.
+//
+//     foo = canFail() catch 6;
+//
+// If canFail() fails, foo will equal 6.
+//
+// const std = @import("std");
+
+// const MyNumberError = error{TooSmall};
+
+// pub fn main() void {
+//     var a: u32 = addTwenty(44) catch 22;
+//     var b: u32 = addTwenty(4) ??? 22;
+
+//     std.debug.print("a={}, b={}\n", .{ a, b });
+// }
+
+// // Please provide the return type from this function.
+// // Hint: it'll be an error union.
+// fn addTwenty(n: u32) ??? {
+//     if (n < 5) {
+//         return MyNumberError.TooSmall;
+//     } else {
+//         return n + 20;
+//     }
+// }
 const std = @import("std");
 
 const MyNumberError = error{TooSmall};
 
 pub fn main() void {
-    var my_number: MyNumberError!u8 = 5;
+    var a: u32 = addTwenty(44) catch 22;
+    var b: u32 = addTwenty(4) catch 22;
 
-    // Looks like my_number will need to either store a number OR
-    // an error. Can you set the type correctly above?
-    my_number = MyNumberError.TooSmall;
+    std.debug.print("a={}, b={}\n", .{ a, b });
+}
 
-    std.debug.print("I compiled!\n", .{});
+fn addTwenty(n: u32) !u32 {
+    if (n < 5) {
+        return MyNumberError.TooSmall;
+    } else {
+        return n + 20;
+    }
 }
