6267afbd830352c24d1a57f4c7ee0f54c6b4c859
stef663k <stef663k@elev.techcollege.dk>
2023-03-07 08:08:54 +0100

    Opgave 27

--- src/main.zig
+++ src/main.zig
@@ -745,7 +745,7 @@
 // program in Zig - one that uses the system Standard Out resource...which
 // can fail!
 //
-const std = @import("std");
+// const std = @import("std");
 
 // Take note that this main() definition now returns "!void" rather
 // than just "void". Since there's no specific error type, this means
@@ -767,8 +767,41 @@ const std = @import("std");
 //     // We just learned of a single statement which can accomplish this.
 //     stdout.print("Hello world!\n", .{});
 // }
-pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
+// pub fn main() !void {
+//     const stdout = std.io.getStdOut().writer();
 
-    try stdout.print("Hello world!\n", .{});
+//     try stdout.print("Hello world!\n", .{});
+// }
+
+//Opgave 27
+//
+// You can assign some code to run _after_ a block of code exits by
+// deferring it with a "defer" statement:
+//
+//     {
+//         defer runLater();
+//         runNow();
+//     }
+//
+// In the example above, runLater() will run when the block ({...})
+// is finished. So the code above will run in the following order:
+//
+//     runNow();
+//     runLater();
+//
+// This feature seems strange at first, but we'll see how it could be
+// useful in the next exercise.
+// const std = @import("std");
+
+// pub fn main() void {
+//     // Without changing anything else, please add a 'defer' statement
+//     // to this code so that our program prints "One Two\n":
+//     std.debug.print("Two\n", .{});
+//     std.debug.print("One ", .{});
+// }
+const std = @import("std");
+
+pub fn main() void {
+    defer std.debug.print("Two\n", .{});
+    std.debug.print("One ", .{});
 }
